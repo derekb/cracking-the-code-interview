dd0a3f737a2262d72f67bda0a62eef76aedeb0fb
derekb <derek.burkgren@gmail.com>
2017-08-28 20:23:57 -0700

    Improve runtime by caching previously seen results.

--- Problems/Chapter8/Problem1.cs
+++ Problems/Chapter8/Problem1.cs
@@ -1,20 +1,27 @@
 using System;
+using System.Collections.Generic;
 
 namespace Problems.Chapter8 
 {
     // Triple Step
     public class Problem1 
     {
-        public static int CountWays(int steps) 
+        public static int CountWays(int steps, IDictionary<int, int> knownWays) 
         {
             if (steps < 0)
                 return 0;
             if (steps == 0)
                 return 1;
-            // Possibilities from 
-            return CountWays(steps - 1) +
-                   CountWays(steps - 2) + 
-                   CountWays(steps - 3);
+
+            if (knownWays.ContainsKey(steps))
+                return knownWays[steps];
+
+            var waysFromHere = CountWays(steps - 1, knownWays) +
+                               CountWays(steps - 2, knownWays) + 
+                               CountWays(steps - 3, knownWays);
+            knownWays.Add(steps, waysFromHere);
+
+            return waysFromHere;
         }
     }
 }
--- Tests/Chapter8/Problem1.cs
+++ Tests/Chapter8/Problem1.cs
@@ -1,5 +1,6 @@
 using Xunit;
 using Problems.Chapter8;
+using System.Collections.Generic;
 
 namespace Problems.Chapter8.Tests
 {
@@ -10,9 +11,10 @@ namespace Problems.Chapter8.Tests
         [InlineData(1, 1)]
         [InlineData(2, 2)]
         [InlineData(3, 4)]
+        [InlineData(4, 7)]
         public void N_Steps_ShoudHave_M_Ways(int steps, int ways)
         {
-            Assert.Equal(ways, Problem1.CountWays(steps));
+            Assert.Equal(ways, Problem1.CountWays(steps, new Dictionary<int, int>()));
         }
     }
 }
